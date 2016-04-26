3c20f324043349de22f840da54ffb5928fe0d6b4
Wes Hediger <wes.hediger@gmail.com>
2016-04-25 23:14:28 -0600

    create for loop to solve and console log series

--- fib.js
+++ fib.js
@@ -1,8 +1,10 @@
 var fib1 = 1;
-var fib2 = 0;
-var stopBefore = 34;
+var fib2 = 1;
+var stopBefore = 3492487944092347840923470948720948723984723094824092472904824709284792385730953529732049875230985735098550983452305987;
 
-for (i = 0; i < stopBefore; i++ ) {
+for (i = 0; fib2 < stopBefore; i++ ) {
   console.log(fib1);
+  console.log(fib2);
   fib1 = fib1 + fib2;
+  fib2 = fib1 + fib2;
 }
